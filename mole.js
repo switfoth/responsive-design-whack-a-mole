09fa662e28c4fbe0a630bed9ef6ab5ebbec93797
Ty <ltgray579@gmail.com>
2020-11-04 11:10:47 -0700

    grid implemented

--- mole.js
+++ mole.js
@@ -1,1 +1,11 @@
 // Write your JS here.
+window.addEventListener('DOMContentLoaded', () => {
+
+    setInterval(() => {
+      const moleHeads = document.querySelectorAll('.mole-head');
+      for (let moleHead of moleHeads) {
+        moleHead.classList.toggle('mole-head-hidden');
+      }
+    }, 1000);
+
+  });
